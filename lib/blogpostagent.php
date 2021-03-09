b2947437603576be80c865f5b7023df8b103e125
Oleg Nizamov <ol.nizamov@yandex.ru>
2021-03-09 15:15:00 +0400

    Доработки

--- lib/blogpostagent.php
+++ lib/blogpostagent.php
@@ -19,45 +19,21 @@ class BlogPostAgent
         Loader::includeModule("blog");
         Loader::includeModule("socialnetwork");
 
-        global $APPLICATION;
-        $flagFindDraftPosts = false;
-
         $dbPosts = \CBlogPost::GetList(
             ['ID' => 'ASC'],
             [
                 'PUBLISH_STATUS' => BLOG_PUBLISH_STATUS_DRAFT,
-                //       '<=DATE_PUBLISH' => date('d.m.Y H:i:s'),
+                '<=DATE_PUBLISH' => date('d.m.Y H:i:s'),
             ]
         );
 
         if ($arPost = $dbPosts->Fetch()) {
-            $flagFindDraftPost = true;
-
             $id = \CBlogPost::Update(
                 $arPost["ID"],
                 [
                     'PUBLISH_STATUS' => BLOG_PUBLISH_STATUS_PUBLISH,
                     "SOCNET_RIGHTS"  => ["UA", "G2"],
                 ]
-            );
-
-            BXClearCache(
-                true,
-                ComponentHelper::getBlogPostCacheDir(
-                    [
-                        'TYPE'    => 'post',
-                        'POST_ID' => $id,
-                    ]
-                )
-            );
-            BXClearCache(
-                true,
-                ComponentHelper::getBlogPostCacheDir(
-                    [
-                        'TYPE'    => 'post_general',
-                        'POST_ID' => $id,
-                    ]
-                )
             );
 
             \CBlogPost::notifyImPublish(
@@ -70,30 +46,8 @@ class BlogPostAgent
             );
         }
 
-        BXClearCache(
-            true,
-            ComponentHelper::getBlogPostCacheDir(
-                [
-                    'TYPE'    => 'posts_popular',
-                    'SITE_ID' => SITE_ID,
-                ]
-            )
-        );
-
-
         return "\Nizamov\BlogPostFormExt\BlogPostAgent::checkDraftPosts();";
     }
-
-    private static function clearBlogPostCache(bool $full = false, array $arr): void
-    {
-        BXClearCache(
-            true,
-            ComponentHelper::getBlogPostCacheDir(
-                $arr
-            )
-        );
-    }
-
 
 }
 
